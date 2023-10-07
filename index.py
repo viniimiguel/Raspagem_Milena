9ffff0d35c6c33936f412485f11a503bbf870675
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2023-10-06 22:55:29 -0300

    second commit

--- index.py
+++ index.py
@@ -17,12 +17,28 @@ class Milena():
 
     def main(self):
         self.abre()
+        self.raspa()
         sleep(131231231)
 
     def abre(self):
         self.driver.get(self.site_link)
         self.driver.maximize_window()
-    
+
+    def raspa(self):
+        site_map = {
+            'XP':{
+                'mais': '#product-search-results > div:nth-child(2) > div.col-sm-12.col-md-9.p > div.row.product-grid.product-tile-plp-space > div.col-12.p-0.grid-footer > div > button'
+            }          
+        }
+        while True:
+            try:
+                mais = self.driver.find_element(By.CSS_SELECTOR, site_map['XP']['mais'])
+                mais.click()
+                sleep(3)
+
+            except:
+                print('nao tem mais resultados')
+        
 
 
 
